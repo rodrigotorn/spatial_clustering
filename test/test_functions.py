c3dd6929b35efe59967f9eefbc96be6226fc5c4e
Rodrigo Tornisiello <rodrigo.torn@outlook.com>
2023-10-22 23:54:20 -0300

    test: Add test for filling and reclustering functions

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -6,7 +7,15 @@ import libpysal as sal
 import src.functions as f
 
 
-def gen_simulated_sectors(n):
+@pytest.fixture
+def simulated_data_with_missing_values():
+  missing_df = gen_simulated_sectors(4)
+  for i in [2, 3, 6, 7, 13]:
+    missing_df.loc[i, 'value'] = np.nan
+  return missing_df
+
+
+def gen_simulated_sectors(n, col_name='value'):
   id_lst = [x for x in range(0, n*n)]
   geometry_lst = []
   value_lst = []
@@ -21,7 +30,7 @@ def gen_simulated_sectors(n):
   gdf = gpd.GeoDataFrame(
     data=pd.DataFrame(
       data={
-        'value': value_lst,
+        col_name: value_lst,
         'geometry': geometry_lst
       },
       index=id_lst
@@ -66,22 +75,63 @@ def test_gen_simulated_sectors():
   )
 
 
-def test_fill_with_neighbors_data():
-  missing_df = gen_simulated_sectors(4)
-  for i in [2, 3, 6, 7, 13]:
-    missing_df.loc[i, 'value'] = np.nan
-
+def test_fill_with_neighbors_data(simulated_data_with_missing_values):
   actual_df = f.fill_with_neighbors_data(
-    df=missing_df,
+    df=simulated_data_with_missing_values,
     agg_dict={'value': 'mean'},
-    weights=sal.weights.Queen.from_dataframe(missing_df)
+    weights=sal.weights.Queen.from_dataframe(simulated_data_with_missing_values)
   )
 
-  expected_df = missing_df
+  expected_df = simulated_data_with_missing_values
   expected_df.loc[2, 'value'] = 35
   expected_df.loc[6, 'value'] = 38
   expected_df.loc[7, 'value'] = 35
   expected_df.loc[13, 'value'] = 54
+
+  pd.testing.assert_frame_equal(
+    actual_df,
+    expected_df
+  )
+
+
+def test_fill_until_limit(simulated_data_with_missing_values):
+  actual_df = f.fill_until_limit(
+    df=simulated_data_with_missing_values,
+    agg_dict={'value': 'mean'},
+    weights=sal.weights.Queen.from_dataframe(simulated_data_with_missing_values)
+  )
+
+  expected_df = simulated_data_with_missing_values
+  expected_df.loc[2, 'value'] = 35
+  expected_df.loc[3, 'value'] = 36
+  expected_df.loc[6, 'value'] = 38
+  expected_df.loc[7, 'value'] = 35
+  expected_df.loc[13, 'value'] = 54
+
+  pd.testing.assert_frame_equal(
+    actual_df,
+    expected_df
+  )
+
+
+def test_recluster_small_clusters():
+  clustered_df = gen_simulated_sectors(3, 'resident_cnt')
+  clustered_df['cluster'] = [
+    1., 2., 3.,
+    1., 2., 2.,
+    1., 1., 1.,
+  ]
+  actual_df = f.recluster_small_clusters(
+    df=clustered_df,
+    min_residents_per_cluster=15,
+    weights=sal.weights.Queen.from_dataframe(clustered_df)
+  )
+  expected_df = clustered_df
+  expected_df['cluster'] = [
+    1., 2., 2.,
+    1., 2., 2.,
+    1., 1., 1.,
+  ]
 
   pd.testing.assert_frame_equal(
     actual_df,
